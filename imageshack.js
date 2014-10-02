117dfb0d9faa35285244b4c0e41851791fa0a292
Andre Lillvede <andre.lillvede@c2.se>
2014-10-02 23:25:33 +0200

    added some more methods

--- imageshack.js
+++ imageshack.js
@@ -1,4 +1,10 @@
-Imageshack = function(username, password, api_key) {
+/**
+ * Imageshack api (http://api.imageshack.us)
+ * Check documentation on above adress for more info on parameters and response models
+ *
+ */
+
+Imageshack = function (username, password, api_key) {
 	var FormData = Npm.require('form-data');
 	Future = Npm.require('fibers/future');
 
@@ -14,25 +20,19 @@ Imageshack = function(username, password, api_key) {
 	}
 
 
-
-	function post(url, params){
+	function post(url, params) {
 		var form = new FormData();
 
-		form.append('api_key',  api_key);
+		form.append('api_key', api_key);
 		form.append('auth_token', auth_token);
 
-		if(Array.isArray(params)){
-			params.forEach(function(param){
-				form.append(param.name, param.value, param.options)
-			})
-		}else if(params){
-			form.append(params.name, params.value, params.options)
-		}
-
+		params.forEach(function (param) {
+			form.append(param.name, param.value, param.options)
+		});
 
 		var fut = new Future();
 
-		form.submit(url, function(err, res) {
+		form.submit(url, function (err, res) {
 			console.log('STATUS: ' + res.statusCode);
 			console.log('HEADERS: ' + JSON.stringify(res.headers));
 
@@ -47,10 +47,10 @@ Imageshack = function(username, password, api_key) {
 				body += chunk;
 			});
 
-			res.on('end', function(){
-				try{
+			res.on('end', function () {
+				try {
 					fut.return(JSON.parse(body).result)
-				}catch(e){
+				} catch (e) {
 					fut.throw(e);
 				}
 			})
@@ -60,66 +60,171 @@ Imageshack = function(username, password, api_key) {
 		return fut.wait();
 	}
 
-	function get(url, params, authenticate){
+	function httpCall(method, url, params, authenticate) {
 
-		if(!authenticate && typeof params == 'boolean'){
+		if (!authenticate && typeof params == 'boolean') {
 			authenticate = params;
 			params = {};
 		}
 
 		params = params || {};
 
-		if(authenticate){
+		if (authenticate) {
 			params.auth_token = auth_token;
 		}
 
-		return JSON.parse(HTTP.get(url, {params: params}).content).result
+		return JSON.parse(HTTP.call(method, url, {params: params}).content).result
 	}
 
-	/*
+	/**
+	 * Upload image
+	 *
+	 *  filename: original filename
+	 *  buffer: node buffer
+	 *  options: an options object containing any or all:
+	 *      - album: Album title or album id to attach files to. If album exists files will be added. If not a new album will be created.
+	 *      - title: Set a title for the image.
+	 *      - description: Set a description for the image.
+	 *      - tags: List of tags to attach to the image. csv
+	 *      - public: Sets public setting. Default is true.
+	 *      - filter: User specified image filter 0-23. 0 is no filter. Will be applied images transloaded.
+	 *      - comments_disabled: Disable comments for the specific image
+	 */
+	this.uploadImage = function (filename, buffer, options) {
+		var params = [{
+			name: 'file@',
+			value: buffer,
+			options: {filename: filename, contentType: 'application/octet-stream'}
+		}];
 
-	- album: Album title or album id to attach files to. If album exists files will be added. If not a new album will be created.
-	- title: Set a title for the image. Use ‘titles[]’ array param or ‘titles’ param separated by commas for multiple image titles.
-	- description: Set a description for the image. Use descriptions[]’ array param or descriptions param separated by commas for multiple image descriptions.
-	- tags: List of tags to attach to the image. Array or csv
-	- public: Sets public setting. Default is true.
-	- filter: User specified image filter 0-23. 0 is no filter. Will be applied images transloaded.
-	- comments_disabled: Disable comments for the specific image
-	*/
-	this.upload = function(filename, buffer, options){
-        var params = [{
-            name: 'file@',
-            value: buffer,
-            options: {filename: filename, contentType: 'application/octet-stream'}
-        }];
-
-        if(options){
-            Object.keys(options).forEach(function(key){
-               params.push({
-                   name: key,
-                   value: options[key],
-               })
-            })
-        }
+		if (options) {
+			Object.keys(options).forEach(function (key) {
+				params.push({
+					name: key,
+					value: options[key],
+				})
+			})
+		}
 		return post('https://api.imageshack.com/v2/images', params);
 	}
 
+	/**
+	 * Transload image(s)
+	 * urls: string of urls to transload
+	 * options: an options object containing any or all:
+	 *      - album: Album title or album id to attach files to. If album exists files will be added. If not a new album will be created.
+	 *      - title or titles: Set a title for the image. csv if titles
+	 *      - description or descriptions: Set description for the image. csv if descriptions
+	 *      - tags: List of tags to attach to the image. csv
+	 *      - public: Sets public setting. Default is true.
+	 *      - filter: User specified image filter 0-23. 0 is no filter. Will be applied images transloaded.
+	 *      - comments_disabled: Disable comments for the specific image
+	 */
+	this.transloadImages = function (urls, options) {
+		var params = [{
+			name: 'urls',
+			value: urls
+		}];
 
-    this.getUserImages = function(username, params){
-        return get('https://api.imageshack.com/v2/user/' + username + '/images', params)
-    }
+		if (options) {
+			Object.keys(options).forEach(function (key) {
+				params.push({
+					name: key,
+					value: options[key],
+				})
+			})
+		}
+		return post('https://api.imageshack.com/v2/images', params);
+	}
 
-	/*
+	/**
+	 * Get image
+	 * id: image id
+	 * options: an options object containing any or all:
+	 *      - next_prev_limit: Sets the limit of next and previous images returned. Default is 2
+	 *      - related_images_limit: Sets the limit of related images to be returned. Default is 10
+	 */
 
-	Params:
-	 hide_empty: Exclude empty albums from album count
-	 show_private: Includes private images in counts
-	 show_hidden: Includes hidden(deleted) images in counts
+	this.getImage = function (id, options) {
+		return httpCall('GET', 'https://api.imageshack.com/v2/images/' + id, options, true)
+	}
 
-	*/
+	/**
+	 * Get multiple images
+	 * ids: string with ids, csv
+	 * options: an options object containing any or all:
+	 *      - next_prev_limit: Sets the limit of next and previous images returned. Default is 2
+	 *      - related_images_limit: Sets the limit of related images to be returned. Default is 10
+	 */
+	this.getMultipleImages = function(ids, options){
+		var params = options || {};
+		params.ids = ids;
+		return httpCall('GET', 'https://api.imageshack.com/v2/images', params, true)
+	}
 
-	this.getUserSettings = function(username, params){
-		return get('https://api.imageshack.com/v2/user/settings', params, true);
+	/**
+	 * id: image id
+	 * options:
+	 *     - title: Set a title for the image
+	 *     - description: Set a description for the image
+	 *     - tags: List of tags to attach to the image. Array or csv
+	 *     - original_filename: Rename the original_filename.
+	 *     - public: Sets public setting. Default is true.
+	 *     - filter: User specified image filter 0-23. 0 is no filter.
+	 *     - comments_disabled: Disable comments for the specific image
+	 */
+	this.updateImage = function(id, options){
+		return httpCall('PUT', 'https://api.imageshack.com/v2/images/' + id, options, true)
+	}
+
+	/**
+	 * ids: string with image ids, csv
+	 * options:
+	 *     - title: Set a title for the image
+	 *     - description: Set a description for the image
+	 *     - tags: List of tags to attach to the image. Array or csv
+	 *     - public: Sets public setting. Default is true.
+	 *     - filter: User specified image filter 0-23. 0 is no filter.
+	 *     - comments_disabled: Disable comments for the specific image
+	 */
+	this.updateMultipleImages = function(ids, options){
+		var params = options || {};
+		params.ids = ids;
+		return httpCall('PUT', 'https://api.imageshack.com/v2/images', params, true)
+	}
+
+	/**
+	 * id: image id of image to delete
+	 */
+	this.deleteImage = function(id){
+		return httpCall('DELETE', 'https://api.imageshack.com/v2/images/' + id, true)
+	}
+
+	/**
+	 * ids: string with ids of images to delete, csv
+	 */
+	this.deleteImage = function(ids){
+		return httpCall('DELETE', 'https://api.imageshack.com/v2/images', {ids: ids}, true)
+	}
+
+
+	/**
+	 * Get user images
+	 * username: get images from this user
+	 * options:
+	 *     - hide_empty: Exclude empty albums from album count
+	 *     - show_private: Includes private images in counts
+	 *     - show_hidden: Includes hidden(deleted) images in counts
+	 *     - limit: amount of images fetched
+	 *     - offset: where in set to start fetching images
+	 */
+	this.getUserImages = function (username, options) {
+		return httpCall('get', 'https://api.imageshack.com/v2/user/' + username + '/images', options, true)
+	}
+
+
+	this.getUserSettings = function (username, params) {
+		return httpCall('get', 'https://api.imageshack.com/v2/user/settings', params, true);
 	}
 
 
